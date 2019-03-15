6c747ac7a7105d9ef5220b0768c5cbedaa319e8e
edgarated <[chukwuebukaaneke22@gmail.com]>
2019-03-15 09:32:55 +0100

    object literals

--- object.js
+++ object.js
@@ -1,6 +1,28 @@
-document.addEventListener("DOMLoadedContent" , function(){
+ //OBJECT LITERALS
+
+  let userOne = {
+    name: "Ebuka",
+    email: "ebuka@outlook.com",
+    login(){
+     console.log(this.email , "has logged in");
+    },
+    logout(){
+      console.log(this.email ,"has logged out");
+    }
+  };
+
+  console.log(userOne.login());
+console.log(userOne.logout());
 
 
 
-  
-})
+
+
+
+
+
+
+
+
+
+
