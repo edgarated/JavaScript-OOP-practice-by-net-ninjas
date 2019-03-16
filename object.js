9021009424d25a4877cdf16a62e14aae03d13ef4
edgarated <[chukwuebukaaneke22@gmail.com]>
2019-03-16 02:50:02 +0100

    Class Constructors

--- object.js
+++ object.js
@@ -1,55 +1,57 @@
- //OBJECT LITERALS
+//CLASS CONTRUCTORS
 
-  let userOne = {
-    name: "Ebuka",
-    email: "ebuka@outlook.com",
-    login(){
-     console.log(this.email , "has logged in");
-    },
-    logout(){
-      console.log(this.email ,"has logged out");
-    }
-  };
-
-//   console.log(userOne.login());
-// console.log(userOne.logout());
+//Example 1
+class User{
+constructor(name, email){
+this.name = name;
+this.email = email;
 
 
-let userTwo = {
-
-name : "ketu",
-occupation: "fishing",
-age: 23,
-email : "ketu@hotmail.com",
-login(){
-  console.log(this.email, "is logged in")
-},
-logout(){
-  console.log(`${this.email}, is logged out`)
 }
+
 }
-//To change property of an object
-userTwo.name = "Kelvin";
-console.log(userTwo.name);
-//prints out Kelvin
-userTwo["name"] = "Kunle";
-console.log(userTwo["name"]);
-//prints out Kunle
-
-//Another way to access the property of an object is by using [] ;
-console.log(userTwo["email"]);
-//prints out ketu@hotmail.com
+const firstOne = new User("kunle" , "ojota@yahoo.com");
+const secondTwo = new User("ekaboo" , "kunfulee@gmail.com")
+console.log(firstOne);
+console.log(secondTwo)
 
 
+//Example 2
+class Detail {
+constructor(name, email,location,age){
+
+this.name = name;
+this.email = email;
+this.location = location;
+this.age = age;
+
+}
+
+}
+const userOne = new Detail("Kunle", "kunle22@hotmail.com", "lagos", 44);
+const userTwo = new Detail("Matthew", "Matt232@outlook.com", "Kano", 23);
+const userThree = new Detail("Mary", "maryonyeali@gmail.com", "Abakaliki", 24);
+console.log(userOne);
+console.log(userTwo);
+console.log(userThree);
 
 
+//Example 3
+class Profile{
 
+  constructor(name,positon,country,club){
+this.name = name;
+this.positon =positon;
+this.country = country;
+this.club = club;
 
+  }
+}
 
+const playerOne = new Profile("Messi" , "Attacker", "Argentina", "Barcelona");
+const playerTwo = new Profile("Ronaldo", "Attacker","Portugal","Juventus");
+const playerThree = new Profile("Hazard", "Attacker", "Belgium", "Chelsea");
 
-
-
-
-
-
-
+console.log(playerOne)
+console.log(playerTwo)
+console.log(playerThree)
