31328aab675df259820cd4c977a4053108f891c8
edgarated <[chukwuebukaaneke22@gmail.com]>
2019-03-17 14:47:17 +0100

    Object chaining

--- object.js
+++ object.js
@@ -1,27 +1,65 @@
-//CLASS METHODS
+//METHOD CHAINING
 
-//Example 
+//Example 1
+
 class User{
 
-  constructor(name, email){
-this.name = name;
+constructor(name, email, location,){
+this.name =name;
 this.email = email;
+this.location = location;
+this.score = 0;
+
+}
+login(){
+  console.log(`${this.email} just logged in`)
+  return this
+}
+logout(){
+  console.log(`${this.email} just logged out`)
+  return this
+};
+
+hot(){
+  console.log(` his location is ${this.location} `);
+  return this;
+}
+updateScore(){
+  this.score++
+  console.log(`${this.score}`);
+  return this;
+}
+}
+const userOne = new User ("kingsley" , "kingsley222@gmail.com", "kenya");
+const userTwo = new User ("Ebuka" , "ebuka@outlook.com", "Abuja");
+
+userOne.login().logout().hot().updateScore();
+userTwo.updateScore()
+userTwo.updateScore()
+userTwo.updateScore()
+userTwo.updateScore()
+
+
+//Example 2
+
+class Hope{
+
+  constructor(subject,name){
+this.subject = subject;
+this.name = name;
+this.score = 0;
   }
-  login(){
-    console.log(`The user with ${this.email} just logged in now`)
-  };
-  logout(){
-    console.log(`The user with ${this.email} just logged out now`)
+  updateScore(){
+    this.score++
+    console.log(`${this.subject} ${this.score}`);
+    return this;
   }
-  fullName(){
-    console.log(`The user ${this.name} is not in our record`);
+  names(){
+    console.log(`${this.name} is not a good man`)
+    return this;
   }
 }
 
-const userOne = new User("Ebuka", "chukwuebukaaneke@outlook.com");
-const userTwo = new User("Ginika", "prettyginika06@gmail.com");
+const userOne = new Hope("Mathematics", "tunde");
 
-userOne.login();
-userTwo.logout();
-userOne.fullName();
-
+userOne.names().updateScore();
