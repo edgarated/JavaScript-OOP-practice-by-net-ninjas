975b17f79f86618962165db4349ec78fadac7085
edgarated <[chukwuebukaaneke22@gmail.com]>
2019-03-18 16:37:11 +0100

    CONSTRUCTORS UNDER THE HOOD

--- object.js
+++ object.js
@@ -1,61 +1,30 @@
-// //CLASS INHERITANCE
+//CONSTRUCTORS UNDER THE HOOD
 
-// //Example 1
-
-class User{
-
-constructor(name, email, location,){
-this.name =name;
+function User( name , email, location){
+this.name = name;
 this.email = email;
 this.location = location;
-this.score = 0;
+
+this.logout = function(){
+ console.log(`${this.email} just logged out`)
+ return this;
 
 }
-login(){
+
+
+this.login = function(){
   console.log(`${this.email} just logged in`)
-  return this
-}
-logout(){
-  console.log(`${this.email} just logged out`)
-  return this
-};
-
-hot(){
-  console.log(` his location is ${this.location} `);
   return this;
-}
-updateScore(){
-  this.score++
-  console.log(`${this.score}`);
-  return this;
-}
-}
-
-// userOne.login().logout().hot().updateScore();
-// userTwo.updateScore()
-// userTwo.updateScore()
-// userTwo.updateScore()
-// userTwo.updateScore()
-
-class Admin extends User{
-  
-deleteUser(user){
-  users = users.filter(u =>{
-   return u.email != user.email;
-  })
-}
-
+ 
+ }
 
 }
-
-
 
 const userOne = new User ("kingsley" , "kingsley222@gmail.com", "kenya");
 const userTwo = new User ("Ebuka" , "ebuka@outlook.com", "Abuja");
-const admin = new Admin ("Adaobi", "Adaobi@gmail.com");
 
-let users = [ userOne , userTwo , admin]
+userOne.logout().login()
+// kingsley222@gmail.com just logged out
+// kingsley222@gmail.com just logged in
 
 
-admin.deleteUser(userOne)
-console.log(users);
